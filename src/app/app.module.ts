353d8b91d86951319ae6c414fbeb79140c1db7a0
Dejan Jankovic <deki1984@hotmail.de>
2023-03-01 16:20:20 +0100

    create search component and results

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/home/components/search/search.component';
+import { ResultsComponent } from './pages/home/components/results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule
